39efebd93463d2df5d148a43763254522bcafa04
linchong <729883852@qq.com>
2018-01-17 12:35:28 +0800

    聊天室

---
+++ src/main/java/com/lin/lcchat/LcchatApplication.java
@@ -0,0 +1,12 @@
+package com.lin.lcchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcchatApplication.class, args);
+	}
+}
