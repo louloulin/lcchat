d0a5babb8cab24e5f122f4cd84ae3d1d4429be57
linchong <729883852@qq.com>
2018-01-19 12:54:28 +0800

    修改

--- src/main/java/com/lin/lcchat/LcchatApplication.java
+++ src/main/java/com/lin/lcchat/LcchatApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class LcchatApplication extends SpringBootServletInitializer {
 
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(LcchatApplication.class);
